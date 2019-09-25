5b686c6ef770615de674be2bab463dca40bf0388
dhruvdua6300 <dhruvdua@mail.usf.edu>
2019-09-24 22:17:32 -0400

    Add project files.

---
+++ Banker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InClass3
+{
+    class Banker : person
+    {
+
+        public Banker(int i) : base(i)
+        {
+            Console.WriteLine("Banker");
+        }
+        person brk = new person(12);
+    }
+}
---
+++ person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace InClass3
+{
+    class person
+    {
+
+        int x, y;
+
+        public person(int x)
+        {
+            this.x = x;
+
+
+        }
+        string firstName = " Dhruv";
+        string lastName = " Dua ";
+        string streetAddress = "Kirti Nagar";
+        string city = "Delhi";
+        string zipCode = "110015";
+        double contactNo = 9503868855;
+
+
+        static void Main(string[] args)
+        {
+           
+            person p = new person(10);
+            
+            person p1 = new person(20);
+           
+            p = p1;
+            person p2 = new person(100);
+            Console.WriteLine(p.x);
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p2.x);
+            Console.ReadLine();
+        }
+
+    }
+}
